c190f406019101278562e5cf09d512ac6a9de946
Alan Marcelo Flores <69022647+AlanMFlores@users.noreply.github.com>
2023-09-21 16:12:59 -0300

    texto justificado

--- src/pages/tasaciones.js
+++ src/pages/tasaciones.js
@@ -17,13 +17,13 @@ const Tasaciones = () => {
           <p className='text-base'>
             Tasaciones Inmobiliarias: Evaluación precisa para tomar decisiones inteligentes
           </p>
-          <p className='text-base'>
+          <p className='text-base text-justify'>
             Cuando confías en San Inmobiliaria para realizar la tasación de tu propiedad, te beneficias de nuestra experiencia y conocimiento profundo del mercado inmobiliario en Posadas y sus alrededores. Nos mantenemos actualizados con las fluctuaciones y tendencias del mercado para brindarte una perspectiva clara y realista sobre el valor de tu propiedad en el contexto actual.
           </p>
-          <p className='text-base'>
+          <p className='text-base text-justify'>
             En San Inmobiliaria, nos comprometemos a ofrecerte un servicio de tasación transparente y profesional. Nuestro equipo estará encantado de responder a todas tus preguntas y brindarte un informe completo que respalde nuestra valoración. Valoramos la confianza que depositas en nosotros y nos esforzamos por superar tus expectativas en cada interacción.
           </p>
-          <p className='text-base'>
+          <p className='text-base text-justify'>
             <Link href={'/contacto'} className='text-main'>¡Contáctanos</Link> hoy mismo y permítenos realizar una tasación inmobiliaria que te brinde una perspectiva clara y precisa del valor de tu propiedad!
           </p>
         </div>
